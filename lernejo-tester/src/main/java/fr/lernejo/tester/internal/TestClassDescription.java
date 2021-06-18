f2c4d1725d36955c48b64b57495c1dfda601c062
Nathan PODESTA <nathan.podesta@icloud.com>
2021-06-18 13:08:05 +0200

    test class method

--- lernejo-tester/src/main/java/fr/lernejo/tester/internal/TestClassDescription.java
+++ lernejo-tester/src/main/java/fr/lernejo/tester/internal/TestClassDescription.java
@@ -8,20 +8,18 @@ import java.util.Arrays;
 import java.util.List;
 
 public class TestClassDescription {
-    Class<?> test;
+    private final Class<?> test;
 
     public TestClassDescription(Class<?> test) {
         this.test = test;
     }
 
     public List<Method> listTestMethods() {
-        List<Method> list =  Arrays.stream(test.getDeclaredMethods())
-            .filter(m -> Modifier.isPublic(m.getModifiers()))
-            .filter(m -> !Modifier.isStatic(m.getModifiers()))
-            .filter(m -> m.getParameterCount() == 0)
-            .filter(m -> m.getReturnType() == void.class)
-            .filter(m -> m.isAnnotationPresent(TestMethod.class))
-            .toList();
-        return list;
+        return Arrays.stream(test.getDeclaredMethods())
+                .filter(m -> Modifier.isPublic(m.getModifiers()))
+                .filter(m -> m.getReturnType().equals(void.class))
+                .filter(m -> m.getParameterCount() == 0)
+                .filter(m -> m.isAnnotationPresent(TestMethod.class))
+                .toList();
     }
 }
--- lernejo-tester/src/test/java/fr/lernejo/tester/internal/TestClassDescriptionLernejoTests.java
+++ lernejo-tester/src/test/java/fr/lernejo/tester/internal/TestClassDescriptionLernejoTests.java
@@ -2,35 +2,25 @@ package fr.lernejo.tester.internal;
 
 import fr.lernejo.tester.SomeLernejoTests;
 import fr.lernejo.tester.api.TestMethod;
+import org.assertj.core.api.Assertions;
 
-public class TestClassDescriptionLernejoTests {
-    public static void main(String[] args) {
-        // call tests fonction
-        test();
-    }
+import java.lang.reflect.Method;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
+class TestClassDescriptionLernejoTests {
     @TestMethod
-    public static void test() {
-        try {
-            var list = new TestClassDescription(SomeLernejoTests.class).listTestMethods();
-            System.out.println(list);
-            assertEq(2, list.size());
+    public void listing_test_methods_annoted() {
+        TestClassDescription desc = new TestClassDescription(SomeLernejoTests.class);
+        List<Method> methods = desc.listTestMethods();
 
-            assertTrue(list.contains(SomeLernejoTests.class.getMethod("ok")));
-            assertTrue(list.contains(SomeLernejoTests.class.getMethod("ko")));
-        } catch (NoSuchMethodException e) {
-            e.printStackTrace();
-            assertTrue(false);
-        }
+        Assertions.assertThat(methods)
+            .extracting(method -> method.getName())
+            .containsExactlyInAnyOrder("ok", "ko");
     }
 
-    public static void assertEq(int expect, int got) {
-        if (expect != got)
-            throw new RuntimeException("Assertion failed, expected " + expect + ", got " + got + "!");
-    }
-
-    public static void assertTrue(boolean test) {
-        if (!test)
-            throw new RuntimeException("Assertion failed (expected true got false)!");
+    public static void main(String[] args) {
+        new TestClassDescriptionLernejoTests().listing_test_methods_annoted();
     }
 }
